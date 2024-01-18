bad420175a00628ac42ce61f0ef3f7132a20c14b
Borja <jesdoborja@gmail.com>
2024-01-18 09:55:40 +0100

    Aplicacion Flask de un chat

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secret'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+
+def handleMessage(msg):
+    print("Message: ", msg)
+    send(msg, broadcast = True)
+
+
+if __name__ == '_main_':
+    socketio.run(app, port=3000, debug=True, host="0.0.0.0")
